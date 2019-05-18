5477a8f19f7b446a9b1a592d57fd9d2b74289da0
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-17 21:28:36 -0500

    clean up app file

--- App.js
+++ App.js
@@ -10,6 +10,12 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <View style={styles.result}></View>
+        <View style={styles.calculation}></View>
+        <View style={styles.buttons}>
+          <View style={styles.numbers}></View>
+          <View style={styles.operations}></View>
+        </View>
       </View>
     );
   }
