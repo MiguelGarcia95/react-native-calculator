222f237d8237f7d6a9790b3b42c71d236d3cccf2
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-18 22:30:31 -0500

    seperate app into components

--- src/components/NumberRow.js
+++ src/components/NumberRow.js
@@ -1,12 +1,41 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet,Text, TouchableOpacity, View } from 'react-native'
 
-const NumberRow = ({number, buttonPressed}) => {
+const displayNumbers = (numbers, buttonPressed) => {
+  return numbers.map(number => {
+    return (
+      <TouchableOpacity key={number} onPress={() => buttonPressed(number)} style={styles.btn}>
+        <Text style={styles.btntext}>{nums[i][j]}</Text>
+      </TouchableOpacity>
+    )
+  })
+}
+
+const NumberRow = ({numbers, buttonPressed}) => {
   return (
-    <View>
-      <Text> textInComponent </Text>
+    <View style={styles.row}>
+      {displayNumbers(numbers, buttonPressed)}
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  btntext: {
+    fontSize: 30,
+    color: 'white'
+  },
+  btn: {
+    flex: 1,
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+});
 
 export default NumberRow;
--- src/components/NumberRows.js
+++ src/components/NumberRows.js
@@ -1,36 +1,25 @@
 import React from 'react'
-import { Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import NumberRow from './NumberRow';
 
-const displayRows = (numbers, buttonPressed) => {
-  return numbers.map(number => {
-    return <NumberRow key={number} numbers={numbers} buttonPressed={buttonPressed} />
+const displayRows = (numbersArray, buttonPressed) => {
+  return numbersArray.map(numbers => {
+    return <NumberRow key={numbers[0]} numbers={numbers} buttonPressed={buttonPressed} />
   })
 }
 
 const NumberRows = ({numbers, buttonPressed}) => {
   return (
     <View style={styles.numbers}>
+      
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  operations: {
-    flex: 1,
-    justifyContent: 'space-around',
-    backgroundColor: '#636363',
-    alignItems: 'stretch',
-  },
-  btntext: {
-    fontSize: 30,
-    color: 'white'
-  },
-  btn: {
-    flex: 1,
-    alignItems: 'center',
-    alignSelf: 'stretch',
-    justifyContent: 'center'
+  numbers: {
+    flex: 3,
+    backgroundColor: '#434343'
   },
 });
 
